f959b825861da79640264e64b180d1983e93e45e
Bhuvy1 <71707237+Bhuvy1@users.noreply.github.com>
2021-03-26 20:14:21 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCb1trKDLGx6kTgw-JEXZSIZSSEhAOaS0g",
+    authDomain: "kwitter-project-842ee.firebaseapp.com",
+    projectId: "kwitter-project-842ee",
+    storageBucket: "kwitter-project-842ee.appspot.com",
+    messagingSenderId: "764601046288",
+    appId: "1:764601046288:web:bd1c7ecb049e5696850195"
+  };
+
+
+ 
+
